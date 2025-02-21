a8d308bbad164556a4f94112e0f5b11316bdc856
kavipriyapaybrook <kavipriya.paybrook@gmail.com>
2025-02-21 12:46:47 +0530

    basic UI created

--- resources/views/admin/organisation.blade.php
+++ resources/views/admin/organisation.blade.php
@@ -1,6 +1,105 @@
 @extends('main')
 @section('organisation')
-
-<p>Organisation</p>
-
+    <div class="card">
+        <div class="card-header">
+            <h5>3 Columns Horizontal Form Layout</h5>
+        </div>
+        <div class="card-body">
+            <form>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <div class="row"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Name:</label>
+                                <div class="col-lg-9"><input type="email" class="form-control" placeholder="Full name">
+                                    <small class="form-text text-muted">Please enter your full name</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <div class="row"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Email:</label>
+                                <div class="col-lg-9"><input type="email" class="form-control" placeholder="Enter email">
+                                    <small class="form-text text-muted">Please enter your email</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <div class="row"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Password:</label>
+                                <div class="col-lg-9">
+                                    <div class="input-group"><span class="input-group-text"><i
+                                                class="feather icon-lock"></i></span> <input type="password"
+                                            class="form-control" placeholder="Enter Password"></div><small
+                                        class="form-text text-muted">Please enter your Password</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-3">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <div class="row"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Contact:</label>
+                                <div class="col-lg-9"><input type="email" class="form-control"
+                                        placeholder="Enter contact number"> <small class="form-text text-muted">Please enter
+                                        your contact</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="row">
+                            <div class="mb-3"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Profile
+                                    URL:</label>
+                                <div class="col-lg-9">
+                                    <div class="input-group search-form"><input type="text" class="form-control"
+                                            placeholder="Fax Profile URL"> <span class="input-group-text bg-transparent"><i
+                                                class="feather icon-alert-circle"></i></span></div><small
+                                        class="form-text text-muted">Please enter Profile URL</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="row"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Pin code:</label>
+                            <div class="col-lg-9"><input type="text" class="form-control"
+                                    placeholder="Enter your Pin code"> <small class="form-text text-muted">Please enter your
+                                    Pin code</small></div>
+                        </div>
+                    </div>
+                </div>
+                <hr class="my-3">
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <div class="row"><label class="col-lg-3 col-form-label text-lg-end px-lg-0">Address:</label>
+                                <div class="col-lg-9"><input type="text" class="form-control"
+                                        placeholder="Enter your address"> <small class="form-text text-muted">Please enter
+                                        your address</small></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="mb-3">
+                            <div class="row align-items-start"><label
+                                    class="col-lg-3 col-form-label text-lg-end px-lg-0 pt-0">User Type:</label>
+                                <div class="col-lg-9">
+                                    <div>
+                                        <div class="form-check form-check-inline"><input type="radio"
+                                                id="customRadioInline231" name="customRadioInline1" class="form-check-input"
+                                                checked="checked"> <label class="form-check-label"
+                                                for="customRadioInline231">Administrator</label></div>
+                                        <div class="form-check form-check-inline"><input type="radio"
+                                                id="customRadioInline232" name="customRadioInline1"
+                                                class="form-check-input"> <label class="form-check-label"
+                                                for="customRadioInline232">Author</label></div>
+                                    </div><small class="form-text">Please select User Type</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,99 +1,95 @@
 <!-- [ Sidebar Menu ] start -->
 <nav class="pc-sidebar">
     <div class="navbar-wrapper">
-      <div class="m-header">
-        <a href="../dashboard/index.html" class="b-brand text-primary">
-          <!-- ========   Change your logo from here   ============ -->
-          <img src="../assets/images/logo-dark.svg" alt="" class="logo logo-lg" />
-        </a>
-      </div>
-      <div class="navbar-content">
-        <ul class="pc-navbar">
-          <li class="pc-item pc-caption">
-            <label>Dashboard</label>
-            <i class="ti ti-dashboard"></i>
-          </li>
-          <li class="pc-item">
-            <a href="{{route('default')}}" class="pc-link"
-              ><span class="pc-micon"><i class="ti ti-dashboard"></i></span><span class="pc-mtext">Default</span></a
-            >
-          </li>
-  
-          <li class="pc-item pc-caption">
-            <label>Elements</label>
-            <i class="ti ti-apps"></i>
-          </li>
-          <li class="pc-item">
-            <a href="{{route('admin.production')}}" class="pc-link">
-              
-              <span class="pc-micon"><i class="ti ti-inbox"></i></span>
-              <span class="pc-mtext">Production System</span>
+        <div class="m-header">
+            <a href="../dashboard/index.html" class="b-brand text-primary">
+                <!-- ========   Change your logo from here   ============ -->
+                <img src="../assets/images/logo-dark.svg" alt="" class="logo logo-lg" />
             </a>
-          </li>
-          <li class="pc-item">
-            <a href="{{route('admin.boundary')}}" class="pc-link">
-              <span class="pc-micon"><i class="ti ti-color-swatch"></i></span>
-              <span class="pc-mtext">Boundary</span>
-            </a>
-          </li>
-          <li class="pc-item pc-caption">
-            <label>Lets Calculate</label>
-            <i class="ti ti-brand-chrome"></i>
-          </li>
-          <li class="pc-item pc-hasmenu">
-            <a href="#!" class="pc-link"
-              ><span class="pc-micon"><i class="ti ti-menu"></i></span><span class="pc-mtext">Menu levels</span
-              ><span class="pc-arrow"><i data-feather="chevron-right"></i></span
-            ></a>
-            <ul class="pc-submenu">
-              <li class="pc-item"><a class="pc-link" href="{{route('admin.organisation')}}">Organisation</a></li>
-              <li class="pc-item pc-hasmenu">
-                <a href="#" class="pc-link"
-                  >Direct GHG<span class="pc-arrow"><i data-feather="chevron-right"></i></span
-                ></a>
-                <ul class="pc-submenu">
-                  <li class="pc-item"><a class="pc-link" href="{{route('admin.direct')}}">Level 3.1</a></li>
-                  <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                  <li class="pc-item pc-hasmenu">
-                    <a href="#!" class="pc-link"
-                      >Level 3.3<span class="pc-arrow"><i data-feather="chevron-right"></i></span
-                    ></a>
-                    <ul class="pc-submenu">
-                      <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                      <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-              <li class="pc-item pc-hasmenu">
-                <a href="#" class="pc-link"
-                  >Indirect GHG<span class="pc-arrow"><i data-feather="chevron-right"></i></span
-                ></a>
-                <ul class="pc-submenu">
-                  <li class="pc-item"><a class="pc-link" href="{{route('admin.indirect')}}">Level 3.1</a></li>
-                  <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
-                  <li class="pc-item pc-hasmenu">
-                    <a href="#!" class="pc-link"
-                      >Level 3.3<span class="pc-arrow"><i data-feather="chevron-right"></i></span
-                    ></a>
-                    <ul class="pc-submenu">
-                      <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
-                      <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-         
-  
-        </ul>
-       
-        <div class="w-100 text-center">
-          <div class="badge theme-version badge rounded-pill bg-light text-dark f-12"></div>
         </div>
-      </div>
+        <div class="navbar-content">
+            <ul class="pc-navbar">
+                <li class="pc-item pc-caption">
+                    <label>Dashboard</label>
+                    <i class="ti ti-dashboard"></i>
+                </li>
+                <li class="pc-item">
+                    <a href="{{ route('default') }}" class="pc-link"><span class="pc-micon"><i
+                                class="ti ti-dashboard"></i></span><span class="pc-mtext">Default</span></a>
+                </li>
+
+                <li class="pc-item pc-caption">
+                    <label>Elements</label>
+                    <i class="ti ti-apps"></i>
+                </li>
+                <li class="pc-item">
+                    <a href="{{ route('admin.production') }}" class="pc-link">
+
+                        <span class="pc-micon"><i class="ti ti-inbox"></i></span>
+                        <span class="pc-mtext">Production System</span>
+                    </a>
+                </li>
+                <li class="pc-item">
+                    <a href="{{ route('admin.boundary') }}" class="pc-link">
+                        <span class="pc-micon"><i class="ti ti-color-swatch"></i></span>
+                        <span class="pc-mtext">Boundary</span>
+                    </a>
+                </li>
+                <li class="pc-item pc-caption">
+                    <label>Lets Calculate</label>
+                    <i class="ti ti-brand-chrome"></i>
+                </li>
+                <li class="pc-item pc-hasmenu">
+                    <a href="#!" class="pc-link"><span class="pc-micon"><i class="ti ti-menu"></i></span><span
+                            class="pc-mtext">Menu levels</span><span class="pc-arrow"><i
+                                data-feather="chevron-right"></i></span></a>
+                    <ul class="pc-submenu">
+                        <li class="pc-item"><a class="pc-link" href="{{ route('admin.organisation') }}">Organisation</a>
+                        </li>
+                        <li class="pc-item pc-hasmenu">
+                            <a href="#" class="pc-link">Direct GHG<span class="pc-arrow"><i
+                                        data-feather="chevron-right"></i></span></a>
+                            <ul class="pc-submenu">
+                                <li class="pc-item"><a class="pc-link" href="{{ route('admin.direct') }}">Level 3.1</a>
+                                </li>
+                                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
+                                <li class="pc-item pc-hasmenu">
+                                    <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i
+                                                data-feather="chevron-right"></i></span></a>
+                                    <ul class="pc-submenu">
+                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
+                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="pc-item pc-hasmenu">
+                            <a href="#" class="pc-link">Indirect GHG<span class="pc-arrow"><i
+                                        data-feather="chevron-right"></i></span></a>
+                            <ul class="pc-submenu">
+                                <li class="pc-item"><a class="pc-link" href="{{ route('admin.indirect') }}">Level
+                                        3.1</a></li>
+                                <li class="pc-item"><a class="pc-link" href="#!">Level 3.2</a></li>
+                                <li class="pc-item pc-hasmenu">
+                                    <a href="#!" class="pc-link">Level 3.3<span class="pc-arrow"><i
+                                                data-feather="chevron-right"></i></span></a>
+                                    <ul class="pc-submenu">
+                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.1</a></li>
+                                        <li class="pc-item"><a class="pc-link" href="#!">Level 4.2</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+
+
+            </ul>
+
+            <div class="w-100 text-center">
+                <div class="badge theme-version badge rounded-pill bg-light text-dark f-12"></div>
+            </div>
+        </div>
     </div>
-  </nav>
-  <!-- [ Sidebar Menu ] end -->
-  
+</nav>
+<!-- [ Sidebar Menu ] end -->
